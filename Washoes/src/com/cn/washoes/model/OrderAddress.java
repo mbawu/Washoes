5a701bf61f532c249ebb725246776c8d395f6c66
Administrator <Administrator@10.9.50.180>
2015-09-11 16:49:55 +0800

    1

---
+++ Washoes/src/com/cn/washoes/model/OrderAddress.java
@@ -0,0 +1,41 @@
+package com.cn.washoes.model;
+
+public class OrderAddress {
+	private String position;
+	private String address;
+	private String mobile;
+	private String realname;
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getRealname() {
+		return realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+
+}
---
+++ Washoes/src/com/cn/washoes/model/SS_Info.java
@@ -0,0 +1,41 @@
+package com.cn.washoes.model;
+
+public class SS_Info {
+	private String ss_duration;
+	private String ss_id;
+	private String ss_price;
+	private String ss_buy_num;
+	private String ss_name;
+	public String getSs_duration() {
+		return ss_duration;
+	}
+	public void setSs_duration(String ss_duration) {
+		this.ss_duration = ss_duration;
+	}
+	public String getSs_id() {
+		return ss_id;
+	}
+	public void setSs_id(String ss_id) {
+		this.ss_id = ss_id;
+	}
+	public String getSs_price() {
+		return ss_price;
+	}
+	public void setSs_price(String ss_price) {
+		this.ss_price = ss_price;
+	}
+	public String getSs_buy_num() {
+		return ss_buy_num;
+	}
+	public void setSs_buy_num(String ss_buy_num) {
+		this.ss_buy_num = ss_buy_num;
+	}
+
+	public String getSs_name() {
+		return ss_name;
+	}
+	public void setSs_name(String ss_name) {
+		this.ss_name = ss_name;
+	}
+	
+}
