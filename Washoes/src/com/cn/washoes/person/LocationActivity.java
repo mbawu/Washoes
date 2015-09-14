8d8f22d30079a299884a2dc2b15e20eb8cbdae78
Wu Jiang <Wu Jiang@10.9.50.148>
2015-09-14 18:01:28 +0800

    1111111111

---
+++ Washoes/src/com/cn/washoes/person/LocationActivity.java
@@ -0,0 +1,16 @@
+package com.cn.washoes.person;
+
+import android.os.Bundle;
+
+import com.cn.hongwei.BaseActivity;
+import com.cn.washoes.R;
+
+public class LocationActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.person_location);
+	}
+}
